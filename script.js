f2e63b7c966c953805857c71ce7aeaa48c087cb9
netronesoftwares <kelvintonyudemy@gmail.com>
2020-08-24 14:37:23 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+var userNamePrompt= prompt("Enter username:");
+var passwordPrompt= prompt("Enter password:");
+
+var database= [
+	{
+		userName: "kelvin",
+		password: "1234"
+	},
+	{
+		userName: "netrone",
+		password: "1234"
+	}
+];
+
+var newsFeed= [
+	{
+		userName: "Emeka",
+		timeline: "so tired from all learning today"
+	},
+	{
+		userName: "chuks",
+		timeline: "javascript is so cool"
+	}
+];
+
+function isUserValid(username, password){
+	for (var i = 0; i < database.length; i++) {
+		if(username===database[i].userName && password===database[i].password){
+			return true;
+		}
+	}
+	return false;
+}
+
+alert("pls always check your console for result");
+
+
+function SignIn(username,password){
+	if(isUserValid(username,password)){
+		for (var i = 0; i < newsFeed.length; i++) {
+			console.log("NewsFeed from "+newsFeed[i].userName+"\n"+"---"+newsFeed[i].timeline);
+		}
+	}else{
+		alert("Oops!, wrong username and password");
+	}
+}
+
+SignIn(userNamePrompt,passwordPrompt);
+
